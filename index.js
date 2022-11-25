547f12263a4f33d05ac5a1abe769aea399d8ae7c
G100g <gigetto@gmail.com>
2022-11-25 21:13:24 +0100

    chore: modernize app

---
+++ index.js
@@ -0,0 +1,29 @@
+import slackin from "./lib/index.js";
+import hostenv from "hostenv";
+
+const config = {
+  port: hostenv.PORT || process.env.PORT || 3000,
+  hostname: hostenv.HOSTNAME || process.env.WEBSITE_HOSTNAME || "0.0.0.0",
+  channels: process.env.SLACK_CHANNELS,
+  interval: process.env.SLACK_INTERVAL || 5000,
+  path: "/",
+
+  token: hostenv.TOKEN || process.env.TOKEN || 3000,
+  org: hostenv.ORG || process.env.ORG || 3000,
+  emails: "",
+  gcaptcha_secret: "111",
+  gcaptcha_sitekey: "111",
+};
+
+console.log(config);
+
+slackin(config).listen(config.port, config.hostname, function (err) {
+  if (err) throw err;
+  //   if (!flags.silent)
+  console.log(
+    "%s – listening on %s:%d",
+    new Date(),
+    config.hostname,
+    config.port
+  );
+});
